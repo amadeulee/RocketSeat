4803f4a4bb75ec69eebf2b569282cc05a99cffdd
Amadeu Lee <76532707+amadeulee@users.noreply.github.com>
2022-08-14 01:57:17 -0300

    config react setting

---
+++ Ignite/ReactJS/01-GitHub-Explorer/src/App.jsx
@@ -0,0 +1,6 @@
+import './styles/global.scss';
+
+export function App() {
+  // throw new Error('babalu');
+  return <h1>Hello World</h1>;
+}
---
+++ Ignite/ReactJS/01-GitHub-Explorer/src/index.jsx
@@ -0,0 +1,6 @@
+import { render } from 'react-dom';
+import { App } from './App';
+
+// render - recebe 2 parametros, primeiro eh o que que quero renderizar, segundo eh de dentro de qual elemento eu quero renderizar
+
+render(<App />, document.getElementById('root'));
