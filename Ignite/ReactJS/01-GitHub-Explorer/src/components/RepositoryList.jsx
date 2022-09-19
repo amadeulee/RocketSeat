8d97e6d123c77e94892642625e9031ed82465884
Amadeu Lee <76532707+amadeulee@users.noreply.github.com>
2022-09-19 20:32:11 -0300

    Fundamentos React + Conceitos

---
+++ Ignite/ReactJS/01-GitHub-Explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,25 @@
+import { RepositoryItem } from './RepositoryItem';
+import './../styles/repositories.scss';
+import { useEffect, useState } from 'react';
+
+//https://api.github.com/orgs/rocketseat/repos
+
+export default function () {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+  }, []);
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositorios</h1>
+      <ul>
+        {repositories.map(repository => (
+          <RepositoryItem key={repository.name} repository={repository} />
+        ))}
+      </ul>
+    </section>
+  );
+}
