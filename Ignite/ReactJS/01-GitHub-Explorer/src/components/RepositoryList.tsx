63d07b7c3ab97842ee56570d90622df6513bbba5
Amadeu Lee <76532707+amadeulee@users.noreply.github.com>
2022-09-19 21:19:43 -0300

    Terminando Fundamentos React

--- Ignite/ReactJS/01-GitHub-Explorer/src/components/RepositoryList.tsx
+++ Ignite/ReactJS/01-GitHub-Explorer/src/components/RepositoryList.tsx
@@ -4,8 +4,14 @@ import { useEffect, useState } from 'react';
 
 //https://api.github.com/orgs/rocketseat/repos
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export default function () {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
